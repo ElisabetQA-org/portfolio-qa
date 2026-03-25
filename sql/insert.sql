e2543c75cbe066c39fb3ad5f1902cd7158cde21c
Elisabet Gacía <elisabet.gar@gmail.com>
2026-03-25 19:39:07 +0000

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,12 +1,12 @@
 INSERT INTO formportfolio (nombre, email, edad, motivo, mensaje)
 VALUES
-('Ana', 'ana@gmail.com', null, 'information', 'hola testing'),
+('ana', 'ana@gmail.com', null, 'information', 'hola testing'),
 (null, 'pedro@gmail.com', 20, 'oferta de trabajo', 'pedro mensaje'),
-('Maria', 'maria@yahoo.com', 25, 'otros', 'mensaje de prueba'),
-('Ana', 'ana.work@gmail.com', 30, 'oferta de trabajo', 'test message'),
+('maria', 'maria@yahoo.com', 25, 'otros', 'mensaje de prueba'),
+('ana', 'ana.work@gmail.com', 30, 'oferta de trabajo', 'test message'),
 (null, 'user1@gmail.com', 22, 'information', 'mensaje testing'),
-(Pedro', 'pedro@test.com', 35, 'otros', 'test mensaje largo'),
-('Juan', 'juan@gmail.com', null, 'oferta de trabajo', 'hola qa testing')
-('Ana', 'ana.test@mail.com', 28, 'informacion', 'mensaje con test'),
+(pedro', 'pedro@test.com', 35, 'otros', 'test mensaje largo'),
+('juan', 'juan@gmail.com', null, 'oferta de trabajo', 'hola qa testing')
+('ana', 'ana.test@mail.com', 28, 'informacion', 'mensaje con test'),
 (null, 'contact@gmail.com', 40, 'oferta de trabajo', 'test email gmail'),
-('Laura', 'laura@mail.com', 19, 'otros', 'mensaje final test');
+('laura', 'laura@mail.com', 19, 'otros', 'mensaje final test');
