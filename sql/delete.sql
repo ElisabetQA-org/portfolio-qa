84ecafe2970073e6314120eda7a8cb054beac0df
Elisabet Gacía <elisabet.gar@gmail.com>
2026-03-31 13:57:46 +0100

    Update delete.sql

--- sql/delete.sql
+++ sql/delete.sql
@@ -1,5 +1,11 @@
 -- Eliminar todos los registros con edad 30.
+ -- consulta
+SELECT * FROM formportfolio WHERE edad = 30;
+ -- borrado
 DELETE FROM formportfolio WHERE edad = 30;
 
 -- Eliminar todos los registros con nombre pedro.
+ -- consulta
+SELECT * FROM formportfolio WHERE nombre = 'pedro';
+ -- borrado
 DELETE FROM formportfolio WHERE nombre = 'pedro';
