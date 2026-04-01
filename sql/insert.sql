5d7593be7ef330d78070a1e1ab6e9e1089458c2c
Elisabet Gacía <elisabet.gar@gmail.com>
2026-04-01 20:11:03 +0100

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -6,7 +6,7 @@ VALUES
 ('ana', 'ana.work@gmail.com', 30, 'oferta de trabajo', 'test message'),
 (null, 'user1@gmail.com', 22, 'information', 'mensaje testing'),
 ('pedro', 'pedro@test.com', 35, 'otros', 'test mensaje largo'),
-('juan', 'juan@gmail.com', null, 'oferta de trabajo', 'hola qa testing')
+('juan', 'juan@gmail.com', null, 'oferta de trabajo', 'hola qa testing'),
 ('ana', 'ana.test@mail.com', 28, 'informacion', 'mensaje con test'),
 (null, 'contact@gmail.com', 40, 'oferta de trabajo', 'test email gmail'),
 ('laura', 'laura@mail.com', 19, 'otros', 'mensaje final test');
